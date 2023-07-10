bff23ad068c5cc9b4c1efc524d79d63d241c0762
00003688 <kadiraydogdu@tcdd.gov.tr>
2023-07-10 23:31:35 +0300

    .

--- SharedLibrary/Dto/ErrorDto.cs
+++ SharedLibrary/Dto/ErrorDto.cs
@@ -8,6 +8,22 @@ namespace SharedLibrary.Dto
 {
     public class ErrorDto
     {
+        public List<string> Errors { get; private set; }
+        public bool IsShow { get; private set; }
+        public ErrorDto() 
+        { 
+            Errors = new List<string>();
+        }
 
+        public ErrorDto(string error,bool isShow)
+        {
+            Errors.Add(error);
+            IsShow = true;
+        }
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            Errors = errors;
+            IsShow = isShow;
+        }
     }
 }
---
+++ UdemyAuthServer.Core/Services/IService.cs
@@ -0,0 +1,20 @@
+﻿using SharedLibrary.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyAuthServer.Core.Services
+{
+    public interface IService<T, TDto> where T : class where TDto : class
+    {
+        Task<Response<TDto>> GetByIdAsync(int id);
+        Task<Response<IEnumerable<TDto>>> GetAllAsync();
+        Task<Response<IEnumerable<TDto>>> Where(Expression<Func<T, bool>> predicate);
+        Task<Response<TDto>> AddAsync(T entity);
+        Task<Response<NoContentDto>>  Remove(T entity);
+        Task<Response<NoContentDto>> Update(T entity);
+    }
+}
